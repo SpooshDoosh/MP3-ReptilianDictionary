8a3a221c23d623d8bc731e34d284cfdd89b97b68
SpooshDoosh <kelvinwnicholson@gmail.com>
2023-02-27 09:04:12 +0000

    Change debug to False

--- app.py
+++ app.py
@@ -171,4 +171,4 @@ def delete_word(word_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
